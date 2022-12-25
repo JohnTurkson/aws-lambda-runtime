3fafeea448c4e8272768ed72a747802ba3492b22
John Turkson <git@johnturkson.com>
2022-12-25 06:09:45 -0800

    Track plugins in rootProject

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,9 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.kotlin.plugin.serialization")
+    id("com.google.devtools.ksp")
+    id("org.graalvm.buildtools.native")
+}
+
 group = "com.johnturkson.aws.runtime"
 version = "1.0-SNAPSHOT"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,9 @@
 pluginManagement {
     plugins {
-        id("org.jetbrains.kotlin.jvm") version "1.7.21" apply false
-        id("org.jetbrains.kotlin.plugin.serialization") version "1.7.21" apply false
-        id("com.google.devtools.ksp") version "1.7.21-1.0.8" apply false
-        id("org.graalvm.buildtools.native") version "0.9.19" apply false
+        id("org.jetbrains.kotlin.jvm") version "1.7.21"
+        id("org.jetbrains.kotlin.plugin.serialization") version "1.7.21"
+        id("com.google.devtools.ksp") version "1.7.21-1.0.8"
+        id("org.graalvm.buildtools.native") version "0.9.19"
     }
     
     repositories {
